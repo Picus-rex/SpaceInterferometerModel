bb293e8e4fb02e51a0897c2d42a37f3336d7cdbe
Picus-Rex <fdb00@yahoo.it>
2025-02-28 17:53:52 +0100

    Fixed nulling ratio computation

--- functions/compute_nulling_ratio.m
+++ functions/compute_nulling_ratio.m
@@ -1,7 +1,7 @@
 function [ratio, rejection] = ...
     compute_nulling_ratio(N, phases, positions, theta_star, lambda)
 
-% Not working check Defrère D. Characterizing extrasolar planetary systems 
+% Defrère D. Characterizing extrasolar planetary systems 
 % using infrared interferometry and Absil O. Astrophysical studies of 
 % extrasolar planetary systems using infrared interferometric techniques 
 % [Internet]. 2006. Available from: 
@@ -9,27 +9,22 @@ function [ratio, rejection] = ...
 
 
 
-
-sum = 0;
+sum = N;  
 for j = 1:N
-    for k = 1:N
-        
+    for k = 1:N        
         if j ~= k
-
             Delta_x = positions(j,1) - positions(k,1);
             Delta_y = positions(j,2) - positions(k,2);
             dcphi = cos(phases(j) - phases(k));
             b_jk = sqrt(Delta_x^2 + Delta_y^2);
             arg = 2*pi * b_jk * theta_star / lambda;
     
-            sum = sum + dcphi * besselj(1, arg) / arg;
-
+            sum = sum + dcphi * (2 * besselj(1, arg) / arg);
         end
-        
     end
 end
 
-ratio = 2/N * sum;
+ratio = sum / N;
 
 rejection = 1/ratio;
 
